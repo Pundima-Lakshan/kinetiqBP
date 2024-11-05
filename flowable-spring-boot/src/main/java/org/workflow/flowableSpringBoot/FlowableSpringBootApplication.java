c1c1a2ea63c8c0d55ae93ec01a07c5d6c7a8d686
Pundima-Lakshan <pundimal@gmail.com>
2024-11-05 14:26:42 +0530

    project structuring

--- flowable-spring-boot/src/main/java/org/workflow/flowableSpringBoot/FlowableSpringBootApplication.java
+++ flowable-spring-boot/src/main/java/org/workflow/flowableSpringBoot/FlowableSpringBootApplication.java
@@ -1,22 +1,13 @@
 package org.workflow.flowableSpringBoot;
 
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.workflow.flowableSpringBoot.service.FlowableUsersService;
 
 @SpringBootApplication(proxyBeanMethods = false)
 public class FlowableSpringBootApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(FlowableSpringBootApplication.class, args);
-    }
-
-    @Bean
-    public CommandLineRunner init(final FlowableUsersService flowableUsersService) {
-
-        return strings -> flowableUsersService.createDemoUsers();
     }
 
 }
