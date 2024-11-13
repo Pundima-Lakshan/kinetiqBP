d0979d68fbc3897cc779eb091970ad7827b08ab1
Pundima Lakshan <pundimal@gmail.com>
2024-11-14 05:25:59 +0530

    Fix eslint configuration

--- kinetiqBP-frontend/eslint.config.js
+++ kinetiqBP-frontend/eslint.config.js
@@ -1,25 +1,46 @@
 import js from '@eslint/js';
-import globals from 'globals';
-import reactHooks from 'eslint-plugin-react-hooks';
-import reactRefresh from 'eslint-plugin-react-refresh';
 import tseslint from 'typescript-eslint';
+import reactPlugin from 'eslint-plugin-react';
+import reactHooksPlugin from 'eslint-plugin-react-hooks';
+import prettierPlugin from 'eslint-config-prettier';
+import globals from 'globals';
 
 export default tseslint.config(
-  { ignores: ['dist'] },
   {
-    extends: [js.configs.recommended, ...tseslint.configs.recommended],
-    files: ['**/*.{ts,tsx}'],
-    languageOptions: {
-      ecmaVersion: 2020,
-      globals: globals.browser,
+    ignores: ['**/.idea/**', '**/dist/**', '**/node_modules/**'],
+  },
+  js.configs.recommended,
+  ...tseslint.configs.recommended,
+  {
+    files: ['**/*.{js,mjs,cjs,jsx,mjsx,ts,tsx,mtsx}'],
+    ...reactPlugin.configs.flat.recommended,
+    rules: {
+      ...reactPlugin.configs.flat.recommended.rules,
+      'react/react-in-jsx-scope': 'off',
     },
+  },
+  {
+    files: ['**/*.{js,mjs,cjs,jsx,mjsx,ts,tsx,mtsx}'],
+    languageOptions: {
+      globals: {
+        ...globals.serviceworker,
+        ...globals.browser,
+      },
+    },
+  },
+  {
+    files: ['**/*.{js,mjs,cjs,jsx,mjsx,ts,tsx,mtsx}'],
     plugins: {
-      'react-hooks': reactHooks,
-      'react-refresh': reactRefresh,
+      'react-hooks': reactHooksPlugin,
     },
     rules: {
-      ...reactHooks.configs.recommended.rules,
-      'react-refresh/only-export-components': ['warn', { allowConstantExport: true }],
+      ...reactHooksPlugin.configs.recommended.rules,
+    },
+  },
+  {
+    files: ['**/*.{js,mjs,cjs,jsx,mjsx,ts,tsx,mtsx}'],
+    rules: {
+      ...prettierPlugin.rules,
     },
   },
 );
--- kinetiqBP-frontend/src/pages/home.tsx
+++ kinetiqBP-frontend/src/pages/home.tsx
@@ -1,4 +1,12 @@
+import { useEffect, useState } from 'react';
+
 export const Home = () => {
+  const [he] = useState();
+
+  useEffect(() => {
+    console.log(he);
+  }, []);
+
   return (
     <div>
       <h1>Home</h1>
