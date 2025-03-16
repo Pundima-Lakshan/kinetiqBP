57266be7cd65bbb8ed28b0de6977e92d2b8a4edf
Pundima-Lakshan <pundimal@gmail.com>
2025-03-17 05:03:57 +0530

    typo fix

--- kinetiqBP-frontend/src/components/organisms/bpmn/common/provider/user-assignment/UserAssignmentPropertiesProvider.js
+++ kinetiqBP-frontend/src/components/organisms/bpmn/common/provider/user-assignment/UserAssignmentPropertiesProvider.js
@@ -1,15 +1,12 @@
-import userAssignmentProps from './parts/userAssignmentProps.js';
+import userAssignmentProps from './parts/UserAssignmentProps.js';
 
 import { is } from 'bpmn-js/lib/util/ModelUtil';
 
 const LOW_PRIORITY = 500;
 
 export default function UserAssignmentPropertiesProvider(propertiesPanel, translate) {
-
-  this.getGroups = function(element) {
-    
-    return function(groups) {
-
+  this.getGroups = function (element) {
+    return function (groups) {
       if (is(element, 'bpmn:UserTask')) {
         groups.push(createUserAssignmentGroup(element, translate));
       }
@@ -17,20 +14,20 @@ export default function UserAssignmentPropertiesProvider(propertiesPanel, transl
       return groups;
     };
   };
-  
+
   propertiesPanel.registerProvider(LOW_PRIORITY, this);
 }
 
-UserAssignmentPropertiesProvider.$inject = [ 'propertiesPanel', 'translate' ];
+UserAssignmentPropertiesProvider.$inject = ['propertiesPanel', 'translate'];
 
 function createUserAssignmentGroup(element, translate) {
-
   const userAssignmentGroup = {
     id: 'userAssignment',
     label: translate('User assignment'),
     entries: userAssignmentProps(element),
-    tooltip: translate('Assign users for the user task')
+    tooltip: translate('Assign users for the user task'),
   };
 
   return userAssignmentGroup;
 }
+
